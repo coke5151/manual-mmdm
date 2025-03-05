26299f8952bb89a562a897bf1d179abe5601e56e
pytree <houjunqimail@gmail.com>
2025-03-05 15:40:22 +0800

    rename db

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./minecraft_mods.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./manual-mmdm.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
